408253bc0fba3ea36313dff1346962ebbaf05579
raafaael12 <111808805+raafaael12@users.noreply.github.com>
2022-12-07 12:38:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+var name = first_name.concat(last_name)
+console.log(name);
+
+var a = 10;
+var b = 5;
+
+var add1 = a + b;
+console.log(add1);
+
+var c = 3;
+var d = 2;
+
+var add2 = c + d;
+console.log(add2);
+
+var e = 32;
+var f = 18;
+
+var add3 = e + f;
+console.log(add3);
+
+var g = 1.3;
+var h = 0.7;
+
+var add4 = g + h;
+console.log(add4);
